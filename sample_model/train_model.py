9f53bd36e873df6b937bd6f95a73b59635be51ef
David Gasquez <davidgasquez@gmail.com>
2016-08-25 20:08:09 +0200

    Create a custom pickled object

--- sample_model/train_model.py
+++ sample_model/train_model.py
@@ -2,6 +2,25 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.externals import joblib
+import cloudpickle
+
+
+def preprocessing(X):
+    return X + 20
+
+
+def dump_model(model, features=None,
+               target_names=None, preprocessing=None):
+    model = {}
+    model['model'] = clf
+    if features:
+        model['features'] = features
+    if target_names:
+        model['target_names'] = target_names
+    if preprocessing:
+        model['preprocessing'] = preprocessing
+
+    return model
 
 # Read Data
 iris = pd.read_csv('iris.csv', header=None,
@@ -21,4 +40,9 @@ clf = RandomForestClassifier()
 clf.fit(X, y)
 
 # Picke classifier
-joblib.dump(clf, 'model.pkl', compress=9)
+final_model = dump_model(clf,
+                         features=list(X.columns),
+                         target_names=list(le.classes_),
+                         preprocessing=cloudpickle.dumps(preprocessing))
+
+joblib.dump(final_model, 'model.pkl', compress=9)

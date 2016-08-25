3ce8d4255f694e227d0cd5ab31d5d34eee1442e9
David Gasquez <davidgasquez@gmail.com>
2016-08-25 14:05:54 +0200

    Split into API and web requests

--- skflask/app.py
+++ skflask/app.py
@@ -2,32 +2,63 @@ from flask import Flask
 from flask import render_template
 from flask import jsonify
 from flask import request
+import os
 from sklearn.externals import joblib
 import pandas as pd
 
-# Load trained model
-clf = joblib.load('model/model.pkl')
+# Available Models
+clfs = {}
 
 # Generate Flask application
 app = Flask(__name__)
 
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        json_data = request.get_json()
-        print(json_data)
-        # X = pd.DataFrame(json_data)
-        # prediction = clf.predict(X)
-        # return jsonify(result=prediction.tolist())
-    else:
+@app.route('/api/models/<name>/predict', methods=['POST'])
+def predict_api(name):
+    """Make a prediction with a model and return the result.
+
+    sample_json = {
+        "column_names": ['a', 'b', 'c']
+        "values": [
+            [1, 4, 9],
+            [2, 0, 6],
+            [1, 4, 8]
+        ]
+    }
+    """
+    json_data = request.get_json()
+    X = pd.Series(json_data)
+    prediction = clfs[name].predict(X.reshape(1, -1))
+    return jsonify(result=prediction.tolist())
+
+
+@app.route('/api/models/<name>', methods=['PUT'])
+def model(name):
+    """Load a model into memory."""
+    tmp_path = '/tmp/{}.plk'.format(name)
+
+    # Write Model to disk
+    with open(tmp_path, 'wb') as f:
+        f.write(request.data)
+
+    # This approach will keep all the models in memory
+    clfs[name] = joblib.load(tmp_path)
+
+    # Remove from disk
+    os.remove(tmp_path)
+
+    return jsonify(status='OK')
+
+
+@app.route('/models', methods=['GET'])
+def list_models():
+        """List loaded models."""
+        return jsonify(list(clfs.keys()))
+
+
+@app.route('/models/<name>/predict', methods=['GET'])
+def predict(name):
         return 'Dynamic Template Here'
-
-
-# TODO
-@app.route('/train')
-def train():
-    return 'Trainig'
 
 
 @app.route('/')

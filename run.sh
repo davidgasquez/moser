21e32ab030f0d84963ab71ea142800ee6284a4c8
David Gasquez <davidgasquez@gmail.com>
2016-08-25 13:47:53 +0200

    Use volumes

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-docker run -it --rm -p 5000:5000 skflask
+if [ "$1" == "dev" ]; then
+    docker run -v $PWD/skflask:/app -it --rm -p 5000:5000 skflask
+else
+    docker run -it --rm -p 5000:5000 skflask
+fi

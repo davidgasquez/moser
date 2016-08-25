c0e64cbb2fade54698aa5bc1d4f1d54463494fd4
David Gasquez <davidgasquez@gmail.com>
2016-08-25 15:42:00 +0200

    Add more tests

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import requests
-import json
 
 # TODO: Use py.test and automate with Travis
 
 base_url = 'http://localhost:5000'
 
+filename = 'sample_model/model.pkl'
+
+# Set the model
+with open(filename, 'rb') as f:
+    model = f.read()
+    r = requests.put(base_url + '/api/models/test', data=model)
+
+# Make a single prediction
 data = {
         'sl': 5.5,
         'sw': 2.8,
@@ -13,5 +20,10 @@ data = {
     }
 
 r = requests.post(base_url + '/api/models/test/predict', json=data)
-
 print(r.json())
+
+# Make several predictions
+with open('sample_model/unknown.csv', 'rb') as f:
+    unknown_data = f.read()
+    r = requests.put(base_url + '/api/models/test/predict', data=unknown_data)
+    print(r.json())

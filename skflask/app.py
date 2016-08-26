db3bb9c8f03c8c8622cd614f3558054817a2ab7a
David Gasquez <davidgasquez@gmail.com>
2016-08-26 12:03:45 +0200

    Enough parallelization for today

--- skflask/app.py
+++ skflask/app.py
@@ -128,4 +128,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(threaded=True, debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)

c3aa6a65ea1e5b7f617c11c1dbd0ece40fdb708a
David Gasquez <davidgasquez@gmail.com>
2016-08-31 16:41:10 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Standard setup file."""
+from setuptools import setup, find_packages
+
+setup(
+    name='moser',
+    version='0.1',
+    description='Airbnb Kaggle competition code.',
+    author='David Gasquez',
+    author_email='davidgasquez@gmail.com',
+    license='MIT',
+    url='https://github.com/davidgasquez/moser',
+    packages=find_packages()
+)
